5ee8ad12afd7d37408fafd5922098ed6fd27f893
Darichel Perez <darichel.perez@gmail.com>
2025-12-01 12:27:52 -0300

    feat/ listen message from server

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -11,8 +11,10 @@ export const connectToServer = () => {
 const addListener = (socket: Socket) => {
   const serverStatuslabel = document.querySelector("#server-status")!;
   const clientsUl = document.querySelector("#clients-ul")!;
- const messageForm = document.querySelector<HTMLFormElement>("#message-form")!;
- const messageInput = document.querySelector<HTMLInputElement>("#message-input")!;
+  const messageForm = document.querySelector<HTMLFormElement>("#message-form")!;
+  const messageInput =
+    document.querySelector<HTMLInputElement>("#message-input")!;
+  let messagesUl = document.querySelector<HTMLUListElement>("#messages-ul")!; 
 
   socket.on("connect", () => {
     serverStatuslabel.innerHTML = "Online";
@@ -23,23 +25,33 @@ const addListener = (socket: Socket) => {
   });
 
   socket.on("clients-updated", (clients: string[]) => {
-    let clientHtml = '';
-    clients.forEach( clientId => {
-      clientHtml += `<li>${ clientId }</li>`;
-    })
+    let clientHtml = "";
+    clients.forEach((clientId) => {
+      clientHtml += `<li>${clientId}</li>`;
+    });
     clientsUl.innerHTML = clientHtml;
   });
 
-  messageForm.addEventListener('submit',  (event) => {
+  messageForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    if(messageInput.value.trim().length <= 0) return;
+    if (messageInput.value.trim().length <= 0) return;
 
-    //console.log({ id: 'Yo', message: messageInput.value });
+    socket.emit("message-from-client", {
+      id: "Yo",
+      message: messageInput.value,
+    });
 
-    socket.emit('message-front-client', { id: 'Yo', message: messageInput.value });
+    messageInput.value = "";
+  });
 
-    messageInput.value = '';
-  })
+  socket.on('message-from-server', (payload: { fullname: string; message: string }) => {
+    const newMessage = `
+      <li>
+        <strong>${payload.fullname}:</strong>
+        <span>${payload.message}</span>
+      </li>
+    `;  
+    messagesUl.innerHTML += newMessage;
+  });
 
-
-};
+}
